6d0bb4e600beb09b0728ae1ece411bd47c8058f4
Pranesh-vimal <pranesh.vimal5@gmail.com>
2021-09-15 10:43:14 +0530

    first push

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -6,7 +6,6 @@ use App\Exports\ImportFile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Maatwebsite\Excel\Facades\Excel;
-use Goutte\Client;
 
 class FileController extends Controller
 {
@@ -17,7 +16,6 @@ class FileController extends Controller
      */
     public function index(Request $request)
     {
-        $client = new Client();
 
         $request->validate([
             'file' => 'required',
@@ -25,37 +23,74 @@ class FileController extends Controller
 
         $array = Excel::toArray(new ImportFile, $request->file);
 
-        $response = Http::asForm()->post('https://results.kongu.edu/xxiofrego.php', [
-            'regno' => '17isr031', 'dob' => '19.03.2000',
-        ]);
-        $dom = new \DOMDocument(); 
-        $html= $response->body();
-        $html=preg_replace('/<!--(.|\s)*?--!>/', '', $html);
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($html);
-        libxml_use_internal_errors(false);
-        $dom->preserveWhiteSpace = false;
-        $tables = $dom->getElementsByTagName('table');
-        $table = $tables->item(1);
-        $rows = $table->getElementsByTagName('tr');
-        $data = [];
-        foreach ($rows as $key => $row) {
-            $info = $row->getElementsByTagName('td');
-            $grade = $row->getElementsByTagName('th');
-            if(isset($grade->item(1)->nodeValue) && isset($grade->item(0)->nodeValue) && $key!=0)
-                $data[$key]['grade']= $grade->item(1)->nodeValue;
-            if(isset($info->item(0)->nodeValue) && isset($info->item(1)->nodeValue) && isset($info->item(2)->nodeValue)){
-                $data[$key]['sem']= $info->item(0)->nodeValue;
-                $data[$key]['subject_code']= $info->item(1)->nodeValue;
-                $data[$key]['subject_name']= $info->item(2)->nodeValue;            
+        $results = [];
+
+        foreach ($array[3] as $key => $value) {
+
+            if ($key == 0) {
+                continue;
             }
+
+            $response = Http::asForm()->post('https://results.kongu.edu/xxiofrego.php', [
+                'regno' => $value[0], 'dob' => $value[2],
+            ]);
+
+            $dom = new \DOMDocument();
+            $html = $response->body();
+            $html = preg_replace('/<!--(.|\s)*?--!>/', '', $html);
+            libxml_use_internal_errors(true);
+            $dom->loadHTML($html);
+            libxml_use_internal_errors(false);
+            $dom->preserveWhiteSpace = false;
+            if ($dom->getElementsByTagName('table')->length > 0) {
+                $tables = $dom->getElementsByTagName('table');
+                $table = $tables->item(1);
+                $rows = $table->getElementsByTagName('tr');
+                $data = [];
+
+                $data['regno'] = $value[0];
+                $data['name'] = $value[1];
+                $data['dob'] = $value[2];
+
+                foreach ($rows as $key => $row) {
+                    $info = $row->getElementsByTagName('td');
+                    $grade = $row->getElementsByTagName('th');
+                    if (isset($info->item(0)->nodeValue) && isset($info->item(1)->nodeValue) && isset($info->item(2)->nodeValue)) {
+                        $data[$key]['sem'] = $info->item(0)->nodeValue;
+                        $data[$key]['subject_code'] = $info->item(1)->nodeValue;
+                        $data[$key]['subject_name'] = $info->item(2)->nodeValue;
+                    }
+                    if (isset($grade->item(1)->nodeValue) && isset($grade->item(0)->nodeValue) && $key != 0) {
+                        $data[$key]['credits'] = $grade->item(0)->nodeValue;
+                        $data[$key]['grade'] = $grade->item(1)->nodeValue;
+                    }
+                }
+            }
+            $results[] = ($data);
         }
-        dd($data);
-        
+        dd($results);
+        // foreach ($results as $key => $value) {
+        //     foreach ($value as $k => $v) {
+        //         if(gettype($v) == 'string'){
+
+        //         }
+        //     }
+        //     dd($value);
+        // }
+
+        // header("Content-Disposition: attachment; filename=\"demo.xls\"");
+        // header("Content-Type: application/vnd.ms-excel;");
+        // header("Pragma: no-cache");
+        // header("Expires: 0");
+        // $out = fopen("php://output", 'w');
+        // foreach ($results as $data) {
+        //     fputcsv($out, $data, "\t");
+        // }
+        // fclose($out);
     }
-    
-    
-    
+
+
+
     /**
      * Show the form for creating a new resource.
      *
